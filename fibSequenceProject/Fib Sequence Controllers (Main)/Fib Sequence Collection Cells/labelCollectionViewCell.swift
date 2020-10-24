a7d91449eb5ee699b1fed5b6777b9df6d73951d9
Elkana Antonio <elkanarahadian@gmail.com>
2020-10-24 22:12:19 +0700

    Update labelCollectionViewCell.swift

--- fibSequenceProject/Fib Sequence Controllers (Main)/Fib Sequence Collection Cells/labelCollectionViewCell.swift
+++ fibSequenceProject/Fib Sequence Controllers (Main)/Fib Sequence Collection Cells/labelCollectionViewCell.swift
@@ -1,15 +1,5 @@
-//
-//  labelCollectionViewCell.swift
-//  fibSequenceProject
-//
-//  Created by Arin Davoodian on 1/22/20.
-//  Copyright © 2020 Arin Davoodian. All rights reserved.
-//
-
 import UIKit
 
-class labelCollectionViewCell: UICollectionViewCell {
-    
-	@IBOutlet weak var fibSequenceUnitLabel: UILabel!
-	
+class labelCollectionViewCell: UICollectionViewCell {    
+	@IBOutlet weak var fibSequenceUnitLabel: UILabel!	
 }
